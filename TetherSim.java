97792a7865ec2128f31e0274dd21f128c9e1190b
Walter Brameld IV <wb4@wbrameld4.name>
2021-04-06 18:59:49 -0400

    SimCanvas class stub

--- TetherSim.java
+++ TetherSim.java
@@ -1,7 +1,12 @@
 
+import java.awt.Dimension;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class TetherSim {
+
+    static private final int VIEW_WIDTH = 2000;
+    static private final int VIEW_HEIGHT = 2000;
 
     public static void main(String[] args) {
         new TetherSim().run();
@@ -10,10 +15,16 @@ public class TetherSim {
     private void run() {
         JFrame frame = new JFrame("TetherSim");
 
+        JComponent canvas = new SimCanvas();
+        canvas.setPreferredSize(new Dimension(VIEW_WIDTH, VIEW_HEIGHT));
+        frame.add(canvas);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
+}
+
+class SimCanvas extends JComponent {
 }
