5eca6326bc9e5059268370a968c9311b3d3dbe0f
Walter Brameld IV <wb4@wbrameld4.name>
2021-04-08 11:37:30 -0400

    Factored out image loading code. Added an image of Earth and code to load it (though it's not displayed yet).

--- TetherSim.java
+++ TetherSim.java
@@ -14,6 +14,7 @@ public class TetherSim {
   private static final int VIEW_HEIGHT = 2000;
 
   private static final File BACKGROUND_IMAGE_FILE = new File("space_background.jpg");
+  private static final File EARTH_IMAGE_FILE = new File("earth.png");
 
   public static void main(String[] args) {
     new TetherSim().run();
@@ -22,13 +23,8 @@ public class TetherSim {
   private void run() {
     JFrame frame = new JFrame("TetherSim");
 
-    BufferedImage backgroundImage = null;
-    try {
-      backgroundImage = ImageIO.read(BACKGROUND_IMAGE_FILE);
-    } catch (IOException exc) {
-      System.err.println("error loading image " + BACKGROUND_IMAGE_FILE + ": " + exc.getMessage());
-      System.exit(1);
-    }
+    BufferedImage backgroundImage = loadImageOrDie(BACKGROUND_IMAGE_FILE);
+    BufferedImage earthImage = loadImageOrDie(EARTH_IMAGE_FILE);
 
     JComponent canvas = new SimCanvas(backgroundImage);
     canvas.setPreferredSize(new Dimension(VIEW_WIDTH, VIEW_HEIGHT));
@@ -37,6 +33,18 @@ public class TetherSim {
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
+  }
+
+  private BufferedImage loadImageOrDie(File imageFile) {
+    BufferedImage image = null;
+    try {
+      image = ImageIO.read(imageFile);
+    } catch (IOException exc) {
+      System.err.println("error loading image " + imageFile + ": " + exc.getMessage());
+      System.exit(1);
+    }
+
+    return image;
   }
 }
 
