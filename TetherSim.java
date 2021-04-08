a5f06e49ef2e6afd696d6586f5a8aeb803ebd608
Walter Brameld IV <wb4@wbrameld4.name>
2021-04-08 10:40:23 -0400

    Centered the background image.  Started using google-java-format to format the code.

--- TetherSim.java
+++ TetherSim.java
@@ -1,63 +1,71 @@
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
-import java.awt.image.BufferedImage;;
-
-import javax.imageio.ImageIO;
-
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
 import java.io.File;
 import java.io.IOException;
-
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class TetherSim {
 
-    static private final int VIEW_WIDTH = 2000;
-    static private final int VIEW_HEIGHT = 2000;
+  private static final int VIEW_WIDTH = 2000;
+  private static final int VIEW_HEIGHT = 2000;
 
-    static private final File BACKGROUND_IMAGE_FILE = new File("space_background.jpg");
+  private static final File BACKGROUND_IMAGE_FILE = new File("space_background.jpg");
 
-    public static void main(String[] args) {
-        new TetherSim().run();
+  public static void main(String[] args) {
+    new TetherSim().run();
+  }
+
+  private void run() {
+    JFrame frame = new JFrame("TetherSim");
+
+    BufferedImage backgroundImage = null;
+    try {
+      backgroundImage = ImageIO.read(BACKGROUND_IMAGE_FILE);
+    } catch (IOException exc) {
+      System.err.println("error loading image " + BACKGROUND_IMAGE_FILE + ": " + exc.getMessage());
+      System.exit(1);
     }
 
-    private void run() {
-        JFrame frame = new JFrame("TetherSim");
+    JComponent canvas = new SimCanvas(backgroundImage);
+    canvas.setPreferredSize(new Dimension(VIEW_WIDTH, VIEW_HEIGHT));
+    frame.add(canvas);
 
-        BufferedImage backgroundImage = null;
-        try {
-            backgroundImage = ImageIO.read(BACKGROUND_IMAGE_FILE);
-        } catch (IOException exc) {
-            System.err.println("error loading image " + BACKGROUND_IMAGE_FILE + ": " + exc.getMessage());
-            System.exit(1);
-        }
-
-        JComponent canvas = new SimCanvas(backgroundImage);
-        canvas.setPreferredSize(new Dimension(VIEW_WIDTH, VIEW_HEIGHT));
-        frame.add(canvas);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
 
 class SimCanvas extends JComponent {
 
-    private BufferedImage backgroundImage;
+  private BufferedImage backgroundImage;
 
-    public SimCanvas(BufferedImage backgroundImage) {
-        this.backgroundImage = backgroundImage;
-    }
+  public SimCanvas(BufferedImage backgroundImage) {
+    this.backgroundImage = backgroundImage;
+  }
 
-    protected void paintComponent(Graphics legacyG) {
-        Graphics2D g = (Graphics2D) legacyG;
+  protected void paintComponent(Graphics legacyG) {
+    Graphics2D g = (Graphics2D) legacyG;
 
-        g.drawImage(backgroundImage, null, 0, 0);
-    }
+    drawBackground(g);
+  }
 
+  private void drawBackground(Graphics2D g) {
+    AffineTransform transform = new AffineTransform();
+    transform.translate(
+        (getWidth() - backgroundImage.getWidth()) / 2,
+        (getHeight() - backgroundImage.getHeight()) / 2);
+
+    BufferedImageOp imageOp =
+        new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+
+    g.drawImage(backgroundImage, imageOp, 0, 0);
+  }
 }
